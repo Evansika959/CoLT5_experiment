0ca4cc757c8530eb69b0b758b0f899dd5474aa4c
Evansika959 <evansjiang0959@gmail.com>
2024-09-24 01:18:07 -0400

    add decoder mask

--- colt5_attention/transformer_block.py
+++ colt5_attention/transformer_block.py
@@ -1144,7 +1144,7 @@ class ConditionalRoutedCrossAttention(nn.Module):
         return out
 
 
-# block
+# encoder block
 
 class ConditionalRoutedTransformerBlock(nn.Module):
     def __init__(
@@ -1209,7 +1209,7 @@ class ConditionalRoutedTransformerBlock(nn.Module):
         num_heavy_ff_tokens = None
     ):
         x = self.conditional_attn(x, mask = mask, num_heavy_tokens_q = num_heavy_attn_tokens_q, num_heavy_tokens_kv = num_heavy_attn_tokens_kv) + x
-        x = self.conditional_ff(x, mask = mask, num_heavy_tokens = num_heavy_ff_tokens) + x
+        x = self.conditional_ff(x, num_heavy_tokens = num_heavy_ff_tokens) + x
         return x
 
 # block for decoder (with cross attention)
@@ -1289,19 +1289,19 @@ class ConditionalRoutedDecoderBlock(nn.Module):
         self,
         x,
         encoder_hidden_states,
-        mask = None,
-        encoder_mask = None,
+        context_mask = None,
         num_heavy_attn_tokens_q = None,
         num_heavy_attn_tokens_kv = None,
         num_heavy_ff_tokens = None
     ):
+        decoder_mask = torch.triu(torch.ones(512, 512), diagonal=1).bool().to('cuda')
         # Self-attention within the decoder block
-        x = self.conditional_self_attn(x, mask=mask, num_heavy_tokens_q=num_heavy_attn_tokens_q, num_heavy_tokens_kv=num_heavy_attn_tokens_kv) + x
+        x = self.conditional_self_attn(x, mask=decoder_mask, num_heavy_tokens_q=num_heavy_attn_tokens_q, num_heavy_tokens_kv=num_heavy_attn_tokens_kv) + x
         
         # Cross-attention with encoder hidden states
-        x = self.conditional_cross_attn(x, context=encoder_hidden_states, mask=mask, context_mask=encoder_mask, num_tokens_q=num_heavy_attn_tokens_q, num_tokens_kv=num_heavy_attn_tokens_kv) + x
+        x = self.conditional_cross_attn(x, context=encoder_hidden_states, mask=decoder_mask, context_mask=context_mask, num_tokens_q=num_heavy_attn_tokens_q, num_tokens_kv=num_heavy_attn_tokens_kv) + x
         
         # Feedforward network
-        x = self.conditional_ff(x, mask=mask, num_heavy_tokens=num_heavy_ff_tokens) + x
+        x = self.conditional_ff(x, num_heavy_tokens=num_heavy_ff_tokens) + x
         return x
 
