29db534ce5b3595c423f17c74720909c5807700e
Evansika959 <evansjiang0959@gmail.com>
2024-09-26 23:53:18 -0400

    add decoder mask

--- colt5_attention/transformer_block.py
+++ colt5_attention/transformer_block.py
@@ -960,11 +960,11 @@ class ConditionalRoutedAutoregressiveAttention(nn.Module):
         window_size = self.heavy_window_size
 
         x, seq_len = pad_to_multiple(x, window_size, dim = -2)
-        print(seq_len.type,seq_len.shape)
+        print(seq_len,seq_len.shape)
         if (mask is not None):
             seq_len = torch.sum(mask, dim=1)
             # print(seq_len,mask)
-        print(seq_len.type,seq_len.shape)
+        print(seq_len,seq_len.shape)
 
         padded_seq_len = x.shape[-2]
 
